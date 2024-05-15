66c419d25e258100a7d7f794d150bbbc24b134fd
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-15 03:46:17 +0100

    function speak

--- jarvis.py
+++ jarvis.py
@@ -1,5 +1,10 @@
 import pyttsx3
 
 engine = pyttsx3.init()
-engine.say("Hello, Sid Ahmed Charfeddine")
-engine.runAndWait()
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+speak('Azzebby')
